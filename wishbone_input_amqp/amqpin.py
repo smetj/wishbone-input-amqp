43fc7e444ff63f2fb9158e5ad9d7b87e9f948251
Jelle Smet <jelle.smet@tomtom.com>
2017-10-09 23:10:38 +0200

    use nucleon library

--- wishbone_input_amqp/amqpin.py
+++ wishbone_input_amqp/amqpin.py
@@ -22,11 +22,18 @@
 #
 #
 
-from gevent import monkey;monkey.patch_all()
+from nucleon.amqp.connection import Connection
+from nucleon.amqp.exceptions import ConnectionError
+from nucleon.amqp.message import Message
 from wishbone import Actor
-from amqp.connection import Connection as amqp_connection
 from gevent import sleep
+from gevent.queue import Empty
 from wishbone.event import Event
+from gevent.hub import Hub
+from gevent.event import Event as g_Event
+
+def print_exception(*args, **kwargs):
+    pass
 
 
 class AMQPIn(Actor):
@@ -98,9 +105,11 @@ class AMQPIn(Actor):
         - prefetch_count(int)(1)
            |  Prefetch count value to consume messages from queue.
 
-        - no_ack(bool)(false)
+        - no_ack(bool)(true)
            |  Override acknowledgement requirement.
 
+        - heartbeat(int)(30)
+           |  Sets the heartbeat interval.
 
     Queues:
 
@@ -119,7 +128,7 @@ class AMQPIn(Actor):
                  exchange_arguments={},
                  queue="wishbone", queue_durable=False, queue_exclusive=False, queue_auto_delete=True, queue_declare=True,
                  queue_arguments={},
-                 routing_key="", prefetch_count=1, no_ack=False):
+                 routing_key="", prefetch_count=1, no_ack=True, heartbeat=30):
         Actor.__init__(self, actor_config)
 
         self.pool.createQueue("outbox")
@@ -128,107 +137,124 @@ class AMQPIn(Actor):
         self.pool.queue.ack.disableFallThrough()
         self.connection = None
 
+        self.go_connect = g_Event()
+        self.go_connect.set()
+        Hub.print_exception = print_exception
+
     def preHook(self):
         self._queue_arguments = dict(self.kwargs.queue_arguments)
         self._exchange_arguments = dict(self.kwargs.exchange_arguments)
-        self.sendToBackground(self.drain)
-        self.sendToBackground(self.handleAcknowledgements)
-        self.sendToBackground(self.handleAcknowledgementsCancel)
+        # self.setupInitialConnection()
+        self.sendToBackground(self.setupConnectivityLoop)
+
+        # self.sendToBackground(self.drain)
+        # self.sendToBackground(self.handleAcknowledgements)
+        # self.sendToBackground(self.handleAcknowledgementsCancel)
 
     def consume(self, message):
-        event = Event(str(message.body))
-        event.set(message.delivery_info["delivery_tag"], "@tmp.%s.delivery_tag" % (self.name))
-        self.submit(event, self.pool.queue.outbox)
 
-    def setupConnectivity(self):
+        if isinstance(message, Message):
+            event = Event(str(message.body))
+            event.set(message.delivery_tag, "@tmp.%s.delivery_tag" % (self.name))
+            self.submit(event, self.pool.queue.outbox)
+        else:
+            if not self.loop():
+                self.logging.error("Problem connecting to broker. Reason: %s" % (event.body))
+                self.go_connect.set()
+
+    def setupConnectivityLoop(self):
 
         while self.loop():
+            self.go_connect.wait()
             try:
-                self.connection = amqp_connection(
-                                    host=self.kwargs.host,
-                                    port=self.kwargs.port,
-                                    virtual_host=self.kwargs.vhost,
-                                    userid=self.kwargs.user,
-                                    password=self.kwargs.password
-                                    )
-                self.connection.connect()
-                self.channel = self.connection.channel()
-
-                if self.kwargs.exchange != "":
-                    self.channel.exchange_declare(
-                        self.kwargs.exchange,
-                        self.kwargs.exchange_type,
-                        durable=self.kwargs.exchange_durable,
-                        auto_delete=self.kwargs.exchange_auto_delete,
-                        passive=self.kwargs.exchange_passive,
-                        arguments=self._exchange_arguments
-                    )
-                    self.logging.debug("Declared exchange %s." % (self.kwargs.exchange))
-
-                if self.kwargs.queue_declare:
-                    self.channel.queue_declare(
-                        self.kwargs.queue,
-                        durable=self.kwargs.queue_durable,
-                        exclusive=self.kwargs.queue_exclusive,
-                        auto_delete=self.kwargs.queue_auto_delete,
-                        arguments=self._queue_arguments
-                    )
-                    self.logging.debug("Declared queue %s." % (self.kwargs.queue))
-
-                if self.kwargs.exchange != "":
-                    self.channel.queue_bind(
-                        self.kwargs.queue,
-                        self.kwargs.exchange,
-                        routing_key=self.kwargs.routing_key
-                    )
-                    self.logging.debug("Bound queue %s to exchange %s." % (self.kwargs.queue, self.kwargs.exchange))
-
-                self.channel.basic_qos(prefetch_size=0, prefetch_count=self.kwargs.prefetch_count, a_global=False)
-                self.channel.basic_consume(self.kwargs.queue, callback=self.consume, no_ack=self.kwargs.no_ack)
-                self.logging.info("Connected to broker.")
+                self.setupConnection()
             except Exception as err:
                 self.logging.error("Failed to connect to broker.  Reason %s " % (err))
                 sleep(1)
             else:
-                break
+                self.go_connect.clear()
 
-    def drain(self):
+    def setupConnection(self):
 
-        self.setupConnectivity()
-        while self.loop():
-            try:
-                self.connection.drain_events()
-                sleep(0)
-            except Exception as err:
-                self.logging.error("Problem connecting to broker.  Reason: %s" % (err))
-                self.setupConnectivity()
-                sleep(1)
+        url = "amqp://%s:%s@%s:%s%s" % (
+            self.kwargs.user,
+            self.kwargs.password,
+            self.kwargs.host,
+            self.kwargs.port,
+            self.kwargs.vhost,
+        )
+
+        self.connection = Connection(
+            amqp_url=url,
+            heartbeat=self.kwargs.heartbeat
+        )
+
+        self.connection.connect()
+        self.channel = self.connection.allocate_channel()
+        self.channel.basic_qos(prefetch_count=self.kwargs.prefetch_count)
+
+        if self.kwargs.exchange != "":
+            self.channel.exchange_declare(
+                self.kwargs.exchange,
+                self.kwargs.exchange_type,
+                durable=self.kwargs.exchange_durable,
+                auto_delete=self.kwargs.exchange_auto_delete,
+                passive=self.kwargs.exchange_passive,
+                arguments=self._exchange_arguments
+            )
+            self.logging.debug("Declared exchange %s." % (self.kwargs.exchange))
+
+        if self.kwargs.queue_declare:
+            self.channel.queue_declare(
+                self.kwargs.queue,
+                durable=self.kwargs.queue_durable,
+                exclusive=self.kwargs.queue_exclusive,
+                auto_delete=self.kwargs.queue_auto_delete,
+                arguments=self._queue_arguments
+            )
+            self.logging.debug("Declared queue %s." % (self.kwargs.queue))
+
+        if self.kwargs.exchange != "":
+            self.channel.queue_bind(
+                self.kwargs.queue,
+                self.kwargs.exchange,
+                routing_key=self.kwargs.routing_key
+            )
+            self.logging.debug("Bound queue %s to exchange %s." % (self.kwargs.queue, self.kwargs.exchange))
+
+        self.channel.basic_consume(
+            queue=self.kwargs.queue,
+            no_ack=self.kwargs.no_ack,
+            callback=self.consume
+        )
+
+        self.logging.info("Connected to broker.")
 
     def handleAcknowledgements(self):
+
         while self.loop():
             event = self.pool.queue.ack.get()
             if event.has("@tmp.%s.delivery_tag" % (self.name)):
                 try:
                     self.channel.basic_ack(event.get("@tmp.%s.delivery_tag" % (self.name)))
                 except Exception as err:
-                    self.pool.queue.ack.rescue(event)
+                    self.submit(event, "ack")
                     self.logging.error("Failed to acknowledge message.  Reason: %s." % (err))
                     sleep(0.5)
             else:
                 self.logging.debug("Cannot acknowledge message because '@tmp.%s.delivery_tag' is missing." % (self.name))
 
     def handleAcknowledgementsCancel(self):
+
         while self.loop():
             event = self.pool.queue.cancel.get()
             try:
                 self.channel.basic_reject(event.get("@tmp.%s.delivery_tag" % (self.name)), True)
             except Exception as err:
-                self.pool.queue.ack.rescue(event)
+                self.submit(event, "cancel")
                 self.logging.error("Failed to cancel acknowledge message.  Reason: %s." % (err))
                 sleep(0.5)
 
     def postHook(self):
-        try:
-            self.connection.close()
-        except:
-            pass
+        self.channel.close()
+        self.connection.close()
